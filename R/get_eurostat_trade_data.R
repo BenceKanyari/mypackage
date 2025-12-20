f8864ddc2af5728a0f98f7e4e732bb9cd38df3bd
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2025-12-20 21:59:09 +0100

    Update get_eurostat_trade_data.R

--- R/get_eurostat_trade_data.R
+++ R/get_eurostat_trade_data.R
@@ -75,28 +75,7 @@ get_eurostat_trade_data <- function(
             "EU27_2020_EXTRA","EU27_2020_INTRA","WORLD"
         )
     } else if("ALL" %in% partner){
-        partner <- c(
-            "AD","AE","AF","AG","AI","AL","AM","AN","AO","AQ","AR","AS","AT","AU","AW","AZ",
-            "BA","BB","BD","BE","BF","BG","BH","BI","BJ","BL","BM","BN","BO","BQ","BR","BS","BT","BV","BW","BY","BZ",
-            "CA","CC","CD","CF","CG","CH","CI","CK","CL","CM","CN","CO","CR","CS","CU","CV","CW","CX","CY","CZ","DD","DE","DJ","DK","DM","DO","DZ",
-            "EA19_EXTRA","EA19_INTRA","EA20_EXTRA","EA20_INTRA","EC","EE","EG","EH","ER","ES","ET",
-            "EU27_2020_EXTRA","EU27_2020_INTRA","EUROZONE_EXTRA","EUROZONE_INTRA","EU_EXTRA","EU_INTRA",
-            "FI","FJ","FK","FM","FO","FR","GA","GB","GD","GE","GF","GH","GI","GL","GM","GN","GP","GQ","GR","GS","GT","GU","GW","GY",
-            "HK","HM","HN","HR","HT","HU","ID","IE","IL","IN","IO","IQ","IR","IS","IT",
-            "JM","JO","JP","KE","KG","KH","KI","KM","KN","KP","KR","KW","KY","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY",
-            "MA","MD","ME","MG","MH","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU","MV","MW","MX","MY","MZ",
-            "NA","NC","NE","NF","NG","NI","NL","NO","NP","NR","NU","NZ",
-            "OM","PA","PE","PF","PG","PH","PK","PL","PM","PN","PS","PT","PW","PY",
-            "QA","QP","QQ","QR","QS","QU","QV","QW","QX","QY","QZ",
-            "RE","RO","RU","RW",
-            "SA","SB","SC","SD","SE","SG","SH","SI","SJ","SK","SL","SM","SN","SO","SR","SS","ST","SU","SV","SX","SY","SZ",
-            "TC","TD","TF","TG","TH","TJ","TK","TL","TM","TN","TO","TP","TR","TT","TV","TW","TZ",
-            "UA","UG","UM","US","UY","UZ",
-            "VA","VC","VE","VG","VI","VN","VU",
-            "WF","WORLD","WS",
-            "XA","XB","XC","XI","XK","XL","XM","XO","XP","XR","XS","XU","XZ",
-            "YD","YE","YT","YU","ZA","ZM","ZW"
-        )
+        partner <- NULL
     }
 
     # --- 2. Generate TIME_PERIOD sequence ---
